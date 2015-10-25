de8019082c54485c79481b14c12224cfaba311d0
Jonas Stenling <jonas@stenling.se>
2015-10-25 21:10:47 +0100

    adding pypi related files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name='pyskate',
+    packages=['pyskate'],
+    version='0.1',
+    description='Netconf abstraction layer for Cisco IOS XE devices',
+    author='Jonas Stenling',
+    author_email='jonas@stenling.se',
+    url='https://github.com/jonasstenling/pyskate',
+    download_url='https://github.com/jonasstenling/pyskate/tarball/0.1',
+    keywords=['netconf', 'Cisco', 'IOS', 'IOS XE', 'ncclient'],
+    classifiers=[]
+)
